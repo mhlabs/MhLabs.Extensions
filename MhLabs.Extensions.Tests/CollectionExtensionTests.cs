c3d1fb78d7e42b40d9088d7fc928ef55937dc889
stefan-karlsson <stefan@successful.se>
2019-03-14 20:05:17 +0100

    feat: IsNullOrEmpty<T>

---
+++ MhLabs.Extensions.Tests/CollectionExtensionTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
+using Xunit;
+
+namespace MhLabs.Extensions.Tests
+{
+    public class CollectionExtensionTests
+    {
+        protected Fixture Fixture = new Fixture();
+        public static IEnumerable<object[]> ListWithEntries;
+
+        public CollectionExtensionTests()
+        {
+            ListWithEntries = Fixture.Create<IEnumerable<object[]>>();
+        }
+
+        public class IsNullOrEmpty : CollectionExtensionTests
+        {
+            [Fact]
+            public void Should_return_true_when_null()
+            {
+                
+                // Arrange
+                IEnumerable<string> collection = null;
+
+                // Act
+                var isNullOrEmpty = collection.IsNullOrEmpty();
+
+                // Assert
+                Assert.True(isNullOrEmpty);
+            }
+
+            [Fact]
+            public void Should_return_true_when_list_is_empty()
+            {
+                var collection = new List<string>();
+                var isNullOrEmpty = collection.IsNullOrEmpty();
+                Assert.True(isNullOrEmpty);
+            }
+
+            [Fact]
+            public void Should_return_false_when_list_has_rows()
+            {
+                var collection = new List<string>() { "row1" };
+                var isNullOrEmpty = collection.IsNullOrEmpty();
+                Assert.False(isNullOrEmpty);
+            }
+        }
+    }
+}
---
+++ MhLabs.Extensions/CollectionExtension.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MhLabs.Extensions
+{
+    public static class CollectionExtension
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> me) => !me?.Any() ?? true;
+    }
+}
