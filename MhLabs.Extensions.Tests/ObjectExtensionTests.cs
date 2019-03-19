c6f3ec3c1bee4b48dab1606c6a4bc80cd0deffb2
Lars Jacobsson <larsj82@gmail.com>
2019-03-19 08:31:14 +0100

    Adding json formatting options

--- MhLabs.Extensions.Tests/ObjectExtensionTests.cs
+++ MhLabs.Extensions.Tests/ObjectExtensionTests.cs
@@ -1,3 +1,4 @@
+using System;
 using AutoFixture;
 using Xunit;
 
@@ -15,11 +16,26 @@ namespace MhLabs.Extensions.Tests
                 Assert.Contains(item.Id, json);
                 Assert.Contains(item.Message, json);
             }
+
             [Fact]
             public void Null_ShouldReturnNull() {
                 TestItem item = null;
                 var json = item.ToJson();
                 Assert.Null(json);
+            }
+
+            [Fact]
+            public void CamelCaseTest() {
+                TestItem item = _fixture.Create<TestItem>();
+                var json = item.ToJson(true);
+                Assert.Contains("\"message\"", json, StringComparison.InvariantCulture);
+            }
+
+            [Fact]
+            public void PascalCaseTest() {
+                TestItem item = _fixture.Create<TestItem>();
+                var json = item.ToJson();
+                Assert.Contains("\"Message\"", json, StringComparison.InvariantCulture);
             }
         }
     }
--- MhLabs.Extensions/ObjectExtension.cs
+++ MhLabs.Extensions/ObjectExtension.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace MhLabs.Extensions
 {
     public static class ObjectExtension
     {
-        public static string ToJson(this object obj) {
-            return obj != null ? JsonConvert.SerializeObject(obj) : null;
+        private static DefaultContractResolver _camelCaseResolver => new DefaultContractResolver
+        {
+            NamingStrategy = new CamelCaseNamingStrategy()
+        };
+
+        public static string ToJson(this object obj, bool camelCase = false, Formatting formatting = Formatting.None)
+        {
+            return obj != null ? JsonConvert.SerializeObject(obj, new JsonSerializerSettings
+            {
+                ContractResolver = camelCase ? _camelCaseResolver : null,
+                Formatting = formatting
+            }) : null;
         }
     }
 }
