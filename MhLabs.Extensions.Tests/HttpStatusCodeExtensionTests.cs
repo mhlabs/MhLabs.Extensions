37a3c59c30e477eaa60b762408ce95a4e070d16e
marcus.casserfelt <marcus.casserfelt@gmail.com>
2021-01-27 08:45:51 +0100

    fix: add httpStatusCode.IsSuccessful extension

---
+++ MhLabs.Extensions.Tests/HttpStatusCodeExtensionTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using Xunit;
+
+namespace MhLabs.Extensions.Tests
+{
+    public class HttpStatusCodeExtensionTests
+    {
+        public class IsSuccessful : HttpStatusCodeExtensionTests
+        {
+            [Theory]
+            [InlineData(200, true)]
+            [InlineData(301, false)]
+            [InlineData(204, true)]
+            [InlineData(500, false)]
+            [InlineData(409, false)]
+            public void Should_Return_True_If_StatusCode_Is_Between_200_And_299(int statusCode, bool expected)
+            {
+                // arrange
+                var httpStatusCode = (HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), statusCode.ToString());
+
+                // act
+                var actual = httpStatusCode.IsSuccessful();
+
+                // assert
+                Assert.Equal(expected, actual);
+            }
+        }
+    }
+}
---
+++ MhLabs.Extensions/HttpStatusCodeExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace MhLabs.Extensions
+{
+    public static class HttpStatusCodeExtension
+    {
+        /// <summary>
+        /// Check if <see cref="HttpStatusCode"/> is between 200 and 299
+        /// </summary>
+        /// <param name="httpStatusCode"></param>
+        public static bool IsSuccessful(this HttpStatusCode httpStatusCode)
+        {
+            if ((int)httpStatusCode >= 200 && (int)httpStatusCode <= 299)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
