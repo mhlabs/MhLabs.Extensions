adf41e4ed4e89f511334fa86365d42a0758e49d5
Marcus Casserfelt <marcus.casserfelt@gmail.com>
2021-10-17 19:55:57 +0200

    fix: added extension method for units

---
+++ MhLabs.Extensions.Tests/UnitExtensionTests.cs
@@ -0,0 +1,64 @@
+﻿using Xunit;
+
+namespace MhLabs.Extensions.Tests
+{
+    public class UnitExtensionTests
+    {
+
+        [Fact]
+        public void Should_Get_MinorUnit_From_Decimal()
+        {
+            // arrange
+            var amount = 100.00M;
+            var expected = 10000;
+
+            // act
+            var actual = amount.ToMinorUnit();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_Get_MinorUnit_From_Double()
+        {
+            // arrange
+            var amount = 100.00D;
+            var expected = 10000;
+
+            // act
+            var actual = amount.ToMinorUnit();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_Get_MajorUnit_In_Decimal_Format()
+        {
+            // arrange
+            var amount = 10000;
+            var expected = 100.00M;
+
+            // act
+            var actual = amount.ToMajorUnit<decimal>();
+            
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_Get_MajorUnit_In_Double_Format()
+        {
+            // arrange
+            var amount = 10000;
+            var expected = 100.00D;
+
+            // act
+            var actual = amount.ToMajorUnit<double>();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ MhLabs.Extensions/UnitExtension.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace MhLabs.Extensions
+{
+    /// <summary>
+    /// Extension method for units
+    /// </summary>
+    public static class UnitExtension
+    {
+        /// <summary>
+        /// Convert double to minor unit
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns>Amount in minor unit</returns>
+        public static int ToMinorUnit(this double amount)
+        {
+            try
+            {
+                var decimalAmount = Convert.ToDecimal(amount);
+                return decimalAmount.ToMinorUnit();
+            }
+            catch 
+            {
+                throw new ArgumentException($"Unable to convert {amount} to MinorUnit");
+            }
+        }
+
+        /// <summary>
+        /// Convert decimal to minor unit
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns>Amount in minor unit</returns>
+        public static int ToMinorUnit(this decimal amount)
+        {
+            try
+            {
+                return (int)Math.Round(amount * 100m);
+            }
+            catch 
+            {
+                throw new ArgumentException($"Unable to convert {amount} to MinorUnit");
+            }
+        }
+
+        /// <summary>
+        /// Convert minor unit to major unit
+        /// </summary>
+        /// <param name="minorUnit"></param>
+        /// <returns>Value in <typeparamref name="T"/></returns>
+        public static T ToMajorUnit<T>(this int minorUnit)
+        {
+            try
+            {
+                var result = minorUnit / 100;
+                return (T)Convert.ChangeType(result, typeof(T));
+            }
+            catch 
+            {
+                throw new ArgumentException($"Unable to covert value to {typeof(T).Name}");
+            }
+        }
+    }
+}
